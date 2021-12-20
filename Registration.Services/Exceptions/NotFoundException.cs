4ca8ccb48e3bc8c31dd2ce6379256c134486866c
Dusan Miljkovic <dusanmiljkovic_95@hotmail.com>
2021-12-20 11:59:07 +0100

    Code refactoring

--- Registration.Services/Exceptions/NotFoundException.cs
+++ Registration.Services/Exceptions/NotFoundException.cs
@@ -1,19 +1,23 @@
 ﻿namespace Registration.Services.Exceptions;
-    public class NotFoundException : Exception
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RacesNotFoundException"/> class.
-        /// </summary>
-        public NotFoundException()
-        {
-        }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RacesNotFoundException"/> class.
-        /// </summary>
-        /// <param name="message">Custom exception message.</param>
-        public NotFoundException(string message)
-            : base(message)
-        {
-        }
+/// <summary>
+/// NotFoundException class.
+/// </summary>
+public class NotFoundException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NotFoundException"/> class.
+    /// </summary>
+    public NotFoundException()
+    {
     }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NotFoundException"/> class.
+    /// </summary>
+    /// <param name="message">Custom exception message.</param>
+    public NotFoundException(string message)
+        : base(message)
+    {
+    }
+}
